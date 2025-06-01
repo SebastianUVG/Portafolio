6cdae4bceed158523bfc3a99c2bc938d78da4704
SebastianUVG <gar22291@uvg.edu.gt>
2025-05-31 19:58:26 -0600

    config styles to the project

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+export const Home = () =>{
+    return <div>
+
+
+
+        {/*Cambio de estilo de pagina */}
+
+        {/* Efectos del fondo*/ }
+
+        {/*NavBar*/}
+
+        {/*Contenido principal*/}
+
+        {/*Footer*/}
+    </div>
+}
