8489b87ef0d55d302f443a80dc1ac06c661fb056
SebastianUVG <gar22291@uvg.edu.gt>
2025-06-01 18:14:06 -0600

    navbar implemented

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from "../components/navBar"
 import { ThemeToggle } from "../components/ThemeToggle"
 import {StarBackground} from "@/components/StarBackground"
 export const Home = () =>{
@@ -9,7 +10,7 @@ export const Home = () =>{
         <StarBackground/>
 
         {/*NavBar*/}
-
+        <Navbar/>
         {/*Contenido principal*/}
 
         {/*Footer*/}
