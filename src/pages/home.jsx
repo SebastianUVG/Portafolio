71376e21e3d1fb8b6ec495091d5ebffb5c3a7164
SebastianUVG <gar22291@uvg.edu.gt>
2025-06-04 12:35:37 -0600

    more bugs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/navBar"
+import { Navbar } from "@/components/NavBar"
 import { ThemeToggle } from "@/components/ThemeToggle"
 import {StarBackground} from "@/components/StarBackground"
 import {HomeSection} from "@/components/HomeSection"
